b25ff403ca5f556fc08759cf174c6340aff191c4
ashwinK555 <ashwinkhatiwada555@gmail.com>
2025-03-25 12:21:54 +0545

    set in python

---
+++ sets/set.py
@@ -0,0 +1,20 @@
+
+collection ={ 1,2,3,"math", True,( 1, 2 )}
+
+collection.add(5)
+
+
+# for x in collection:
+#     print(x)
+
+# collection.remove(True)
+# print(collection)
+
+# collection.pop()
+# print(collection)
+
+student ={"sci", "math"}
+student2 = {"sci","bio"}
+
+print(student.union(student2))
+print(student.intersection(student2))
